d3cbfa2c2359e4c716110f60e3fb38da2999c682
Vlad <vpanovwr@gmail.com>
2026-02-20 11:03:17 +0200

    setup(validation): new validation flow

---
+++ src/Helpers/ValidationHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+use App\DTO\RequestContract;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ValidationHelper
+{
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function validate(Request $request, RequestContract $dtoClass): object
+    {
+        $data = json_decode($request->getContent(), true) ?? [];
+
+        $dto = $dtoClass::fromArray($data);
+
+        $errors = $this->validator->validate($dto);
+
+        if (count($errors) > 0) {
+            $messages = [];
+            foreach ($errors as $error) {
+                $messages[$error->getPropertyPath()] = $error->getMessage();
+            }
+
+            $errString = implode(", \r\n", $messages);
+            throw new ValidationException($errString);
+        }
+
+        return $dto;
+    }
+}
